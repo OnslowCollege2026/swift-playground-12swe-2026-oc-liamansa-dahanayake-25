0d66ee5ee381d62df744fff9c389c3b058be23d4
oc-liamansa-dahanayake-25 <limansa.sepaladahana@student.onslow.school.nz>
2026-02-24 00:11:50 +0000

    2D Array

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,142 +1,88 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+func print(board: [[String]]) {
+    board.forEach { line in
+        print("| \(line[0]) | \(line[1]) | \(line[2]))")
+        print("--+---+--")
+    }
+    print()
+}
+
+func askForPosition(board: [[String]]) -> [Int] {
+    while true {
+    print("Please enter the row number: ")
+    let userInput = readLine()!
+    let rowNumber = Int(userInput)!
+
+    print("Please enter the row number 1-3:")
+    let userInput2 = readLine()!
+    let columnNumber = Int(userInput)!
+
+    if board[rowNumber][columnNumber] == "." {
+        return [rowNumber, columnNumber]
+        }
+    }
+}
 
 
 @main
 struct SwiftPlayground {
     static func main() {
+        var player = "O"
+        var board = [
+            [".",".","."], //this is row 0
+            [".",".","."], //this is row 1
+            [".",".","."], //this is row 2
+        ]
+        print(board: board)
 
-var eggInStock = 0
-var eggsSold = 0
-let maxAmount = 1000
-let lowStock = 50
-
-func menuName() -> Int {
-    print("""
-    ==== Egg Shop ====
-    1. Add eggs
-    2. Sell eggs
-    3. Show current stock
-    4. Show total eggs sold
-    5. Exit
-    Choose an option:
-    """)
-
-guard let input = readLine(), let choice = Int(input) else {
-    print("Invalid input. Please enter a number.")
-    return 0
-    }
-    return choice
-}
+        // Ask for the user's move.
+        let position = askForPosition(board: board)
+        board[position[0]] [position[1]] = player
 
 
-func theQuantity() -> Int {
-    guard let input = readLine(), let amount = Int(input) else {
-        print("Invalid input. please enter a whole number.")
-        return nil
-    }
-    if amount < 1 {
-        print(" Quantity must be at least 1. Please try again.")
-        return nil
-    }
-}
+        print(board: board)
 
-func addEggs(currentStock: Int, amount: Int) -> Int {
-    let newStock = currentStock + amount
-    if newStock > maxAmount {
-        print("Cannot exceed \(maxAmount) eggs inn stock.")
-    }
-    return newStock
-}
+        if player == "O" {
+            player = "X"
+        }else { 
+            player = "O"
 
-
-
-func sellEggs(currentStock: Int, amount: Int) -> Int? {
-    if amount < currentStock {
-        print("Cannot sell more than eggs than are in stock.")
-    }
-    return currentStock - amount
-}
-
-//func updateSoldCount(currentSold: Int, amount: Int) -> Int {
-    //eturn currentSold + amount
-//}
-
-func stockMassage(stock: Int) -> String {
-    if stock == 0 {
-        return "Stock is empty. You have no eggs."
-    } else if stock < lowStock {
-        return "Current \(stock) eggs. Low in stock."
-    } else {
-        return "Current \(stock) eggs."
-    }
-}
-
-func eggsSold(totalSold: Int) -> String {
-    return "Total eggs sold today is \(totalSold)."
-}
-
-func resetShop() {
-    eggsInStock = 0
-    eggsSold = 0
-    print("Shop has been reset to a new day. The stock and the sold amount is reseted.")
-}
-
-
-
-
-var running = true
-
-while running {
-    let choice = menuName()
-
-
-switch choice {
-    case 1:
-    if let amount = theQuantity(prompt: "How many eggs would you like to add?") {
-        if let newStock = addEggs(currentStock: eggsInStock, amount: amount) {
-            eggInStock = newStock
-            print(stockMassage: eggsInStock)
         }
+
+
+
+
+
+
+
+
+        // First move: 0 in middle.
+        board[1][1] = "O"
+        print(board: board)
+
+        // Second move: X in top-left
+        board[0][0] = "X"
+        print(board: board)
+        
+        // Third move: 0 in top-right
+        board[0][2]
+        print(board: board)
+
+        //Forth move: X in bottom-left
+        board[2][0] = "O"
+
+        // Fifth move: 0 in middle-right
+        board[1][0] = "X"
+        print(board: board)
+
+        // Sixth move: O in bottom- middle
+        board[2][1] = "O"
+        print(board: board)
+        
+        // seventh move: X in bottom- middle
+        board[0][1] = "X"
+        print(board: board)
     }
-
-
-    case 2:
-    if let amount = theQuantity(prompt: "how many eggs would you like to sell? ") {
-        if let newStock = sellEggs(currentStock: eggsInStock, amount: amount) {
-            eggInStock = newStock
-            eggsSold = updateSoldCount(current: eggsSold, amount: amount) 
-            print("Sale completed.")
-            print(stockMessage(stock: eggsInStock))
-            print(soldMessage(totalSold: eggsSold))
-        }
-    }
-
-
-    case 3:
-    print(stockMassage:(stock: eggsInStock))
-
-    case 4: 
-    print(soldMessage(totalSold: eggsSold))
-
-
-    case 5:
-    print("Exiting egg Shop. Goodbye!")
-    running = false
-
-    case 6:
-    resetShop()
-
-    case  0:
-    continue
-
 }
-}
-
-
-
-
-
-}
-    }
